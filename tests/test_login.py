d7740edf589ccd5ba5d57682adb117587f88222a
Jaber Rammal  - Software QA Manual and automation <66329889+jaber114@users.noreply.github.com>
2026-01-26 15:44:53 +0200

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -13,22 +13,22 @@ class Test_login(BaseTest):
     @pytest.mark.parametrize("username,password",users)
     def test_login(self,username,password):
        with allure.step("Clicking on the drop down button"):
-        self.login_page.delay(2)
-        self.login_page.click_on_my_account_drop_down()
+         self.login_page.delay(2)
+         self.login_page.click_on_my_account_drop_down()
        with allure.step("Clicking on Login button the maim menu"):
-        self.login_page.delay(2)
-        self.login_page.menu_login_button()
+         self.login_page.delay(2)
+         self.login_page.menu_login_button()
        with allure.step("Filling the email and password with the data above"):
-        self.login_page.delay(2)
-        self.login_page.fill_login_fields(username,password)
+         self.login_page.delay(2)
+         self.login_page.fill_login_fields(username,password)
        with allure.step("Clicking on the login button"):
-        self.login_page.delay(2)
-        self.login_page.login()
-        self.login_page.delay(2)
-        if self.login_page.login_error_message():
+         self.login_page.delay(2)
+         self.login_page.login()
+         self.login_page.delay(2)
+         if self.login_page.login_error_message():
             assert True
-        else:
-          assert False ,"Failed to login to the website"
+         else:
+           assert False ,"Failed to login to the website"
 
 
 
