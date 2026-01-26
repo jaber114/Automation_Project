fa4f6eae8a71b029b1e27fa33b1505ccac2b8389
Jaber Rammal  - Software QA Manual and automation <66329889+jaber114@users.noreply.github.com>
2026-01-26 15:58:37 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,6 +17,13 @@ def setup(request):
         case "Chrome":
           request.cls.driver = webdriver.Chrome()
         case "Edge":
+          opts = Options()
+          opts.add_argument("--user-data-dir=C:\\ProgramData\\Jenkins\\edge-profile")
+          opts.add_argument("--no-first-run")
+          opts.add_argument("--no-default-browser-check")
+          opts.add_argument("--disable-dev-shm-usage")
+          opts.add_argument("--disable-gpu")
+          opts.add_argument("--window-size=1920,1080")        
           request.cls.driver = webdriver.Edge()
         case "Firefox":
           request.cls.driver=webdriver.Firefox()
