d00c5bd3d9e7c65242d9d3b7e0837edc097fa13a
Jaber Rammal  - Software QA Manual and automation <66329889+jaber114@users.noreply.github.com>
2026-01-27 16:35:41 +0200

    Update base_test.py

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,14 +1,16 @@
-from pages.check_out_forms import CheckOutForms
-from pages.choose_product import ChooseProduct
-from pages.login_page import LoginPage
-from pages.sign_up_page import SignUp
-from pages.user_actions import UserActions
+from __future__ import annotations
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from pages.check_out_forms import CheckOutForms
+    from pages.choose_product import ChooseProduct
+    from pages.login_page import LoginPage
+    from pages.sign_up_page import SignUp
+    from pages.user_actions import UserActions
 
 class BaseTest:
-
- login_page : LoginPage
- sign_up_page : SignUp
- user_actions :UserActions
- choose_product_page :ChooseProduct
- check_out_forms : CheckOutForms
+    login_page: "LoginPage"
+    sign_up_page: "SignUp"
+    user_actions: "UserActions"
+    choose_product_page: "ChooseProduct"
+    check_out_forms: "CheckOutForms"
