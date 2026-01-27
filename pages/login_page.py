9297ec36b9847776132fd837cdcac97f091cad3f
Jaber Rammal  - Software QA Manual and automation <66329889+jaber114@users.noreply.github.com>
2026-01-27 13:04:53 +0200

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -21,7 +21,10 @@ class LoginPage(BasePage):
 
 
     def click_on_my_account_drop_down(self):
+        self.delay(2)
         self.click(self.MY_ACCOUNT_DROP_DOWN)
+        self.delay(2)
+
 
     def menu_login_button(self):
         self.click(self.ACCOUNT_DROP_DOWN_LOGIN_BUTTON)
